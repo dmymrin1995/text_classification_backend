7e31ce6c8e468a56f8958ca1bcb628f6f5161cb3
dmymrin1995 <dmymrin1995@gmail.com>
2023-09-23 20:46:51 +0800

    add request method

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from get_predict import ModelPredict
 
 app = Flask(__name__)
 model = ModelPredict()
 
-model.load_model()
 
-
-@app.route('/<string:review>')
+@app.route('/review_predict/<string:review>', methods = ['GET', 'POST'])
 def wait_string(review):
-    rev = {review}
-    rev_list =[]
-    for s in rev:
-        rev_list.append(s)
-    prediction = model.get_predict(rev_list)
-    return jsonify(prediction.tolist())
+    if request.method == 'POST':
+        rev = {review}
+        rev_list =[]
+        for s in rev:
+            rev_list.append(s)
+        prediction = model.get_predict(rev_list)
+        return jsonify(prediction.tolist())
+
 
 if __name__ == '__main__':
+    model.load_model()
     app.run()
