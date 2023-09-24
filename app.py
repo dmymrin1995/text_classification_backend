fab1ca2797df2e1b918eec539d5913b64c66d3c7
dmymrin1995 <dmymrin1995@gmail.com>
2023-09-24 23:59:46 +0800

    типа api

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 model = ModelPredict()
 
 
-@app.route('/review_predict/<string:review>', methods = ['GET', 'POST'])
+@app.route('/api/sreview_predict/<string:review>', methods = ['GET', 'POST'])
 def wait_string(review):
     if request.method == 'POST':
         rev = {review}
         rev_list =[]
         for s in rev:
+            
             rev_list.append(s)
         prediction = model.get_predict(rev_list)
-        return jsonify(prediction.tolist())
+        prediction = [x for x in prediction.tolist()]
+        pred_dictionary = {"prediction": prediction}
+        return jsonify(pred_dictionary)
 
 
 if __name__ == '__main__':
